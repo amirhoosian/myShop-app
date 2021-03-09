735adbe5a51bdb9e73a2bbd821db4e5690a5fc68
mrdcode78 <mrdcode78@gmail.com>
2021-03-09 21:19:17 +0330

    v1 test

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const path = require('path')
 const adminRoute = require('./route/admin')
 const shopRoute = require('./route/shop')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoute)
 app.use(shopRoute)
 
 app.use((req, res) => {
-    res.status(404).send('page cant bee found')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 
--- route/admin.js
+++ route/admin.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const path = require('path')
+
+const rootDir = require('../util/path')
 
 const router = express.Router()
 
 
 
 router.get('/add-product', (req, res) => {
-    res.send('<form method="POST" action="/admin/add-product"><input type="text" name="title"/> <button type="submit">send</button></form>')
-    
+    res.sendFile(path.join(rootDir ,'views', 'add-product.html'))
 })
 router.post('/add-product', (req, res) => {
     console.log(req.body)
