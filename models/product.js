b74c8c2619313484fceda757c2818fe39f518368
mrdcode78 <mrdcode78@gmail.com>
2021-06-11 20:10:05 +0430

    refactor

--- models/product.js
+++ models/product.js
@@ -3,35 +3,34 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
+const p = path.join(rootDir, 'Data', 'products.json')
+
+const getProductFromFIle = cb => {
+   
+        fs.readFile(p, 'utf-8', (err, data) => {
+           let myData = data === '' ? [] : JSON.parse(data)
+           cb(myData)
+            
+        })
+}
 class product{
     constructor(title) {
         this.title = title
     }
 
     save(){
-        const p = path.join(rootDir, 'Data', 'products.json')
-        fs.readFile(p, (err, data) => {
-            let product = []
-            if(!err){
-                try {
-                    product = JSON.parse(data)
-                } catch (error) {
-                    product = []
-                }
-              
-            }
+        getProductFromFIle(product => {
             product.push(this)
-            fs.writeFile(p, JSON.stringify(product), (err) => {console.log(err)})
+            fs.writeFile(p, JSON.stringify(product), (err) => {
+                if(err != null){
+                console.log(err)}
+            })
         })
+       
     }
 
     static fetchAll(cb){
-        const p = path.join(rootDir, 'Data', 'products.json')
-        fs.readFile(p, 'utf-8', (err, data) => {
-           let myData = data === '' ? [] : JSON.parse(data)
-           cb(myData)
-            
-        })
+        getProductFromFIle(cb)
     }
 }
 
